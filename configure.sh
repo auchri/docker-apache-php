d23d77437992f6bcb1e570452441df80b646e6a9
Christoph <auchri@users.noreply.github.com>
2016-07-28 20:33:38 +0200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 [[ ! $TIMEZONE ]] && TIMEZONE="Europe/Vienna"
+PHP_INI="/etc/php5/apache2/php.ini"
 
-echo "date.timezone = ${TIMEZONE}" >> "/etc/php5/apache2/php.ini"
+sed -i "s_;date.timezone =_date.timezone = ${TIMEZONE}_g" "$PHP_INI"
 
 /usr/sbin/apache2ctl -D "FOREGROUND"
